f1dcdec759ce2082da061d005c1f31892601f9e9
Dennis Martin <demargin2409@gmail.com>
2024-01-02 18:49:56 -0500

    updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
+    
     {
         // \App\Models\User::factory(10)->create();
 
@@ -24,5 +25,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
 
         ]);
+
+        
     }
 }
